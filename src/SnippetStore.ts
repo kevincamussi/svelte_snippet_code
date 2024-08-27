f00de7152037fc6e64fc17a421c4ebe69a470d8a
kevincamussi <kevincamussi@gmail.com>
2024-08-27 19:43:34 -0300

    feat: create toggleFavorite function inside store

--- src/SnippetStore.ts
+++ src/SnippetStore.ts
@@ -4,16 +4,32 @@ export const snippetStore = writable<CodeSnippet[]>([]);
 
 export const addSnippet = (input: CodeSnippetInput) => {
 	const snippets = get(snippetStore);
+
 	snippetStore.update(() => {
+		// Codesnippet []
 		return [{ ...input, favorite: false }, ...snippets];
 	});
 };
 
 export const deleteSnippet = (index: number) => {
 	const snippets = get(snippetStore);
+
 	snippets.splice(index, 1); // removes 1 item from index
 	snippetStore.update(() => {
 		// codeSnippet
 		return snippets;
+	});
+};
+
+export const toggleFavorite = (index: number) => {
+	const snippets = get(snippetStore);
+
+	snippetStore.update(() => {
+		return snippets.map((snippet, snippetIndex) => {
+			if (snippetIndex === index) {
+				return { ...snippet, favorite: !snippet.favorite };
+			}
+			return snippet;
+		});
 	});
 };
