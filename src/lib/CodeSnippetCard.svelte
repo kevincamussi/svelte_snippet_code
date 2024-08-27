c942007ed6b3f8b6de5d43f7a94b463f6fcc310c
kevincamussi <kevincamussi@gmail.com>
2024-08-27 19:27:20 -0300

    First commit

---
+++ src/lib/CodeSnippetCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="card">
+	<header class="card-header">
+		Title Of Snippet
+		<div class="float-right">
+			<button class="btn btn-sm variant-filled-secondary" type="button"> Favorite </button>
+			<button class="btn btn-sm variant-filled-error" type="button"> X </button>
+		</div>
+	</header>
+	<section class="p-4">
+		<CodeBlock language={'HTML'} code={`<h1>Hey</h1>`} />
+	</section>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import CodeSnippetCard from '$lib/CodeSnippetCard.svelte';
+
+	let formData: CodeSnippetInput = {
+		title: '',
+		language: 'html',
+		code: ''
+	};
+
+	//createSnippet(input: CodeSnippetInput)
+	//SnippetStore -> local writable that allows us to store code snippets
+	//favorite snippets
+	//+page.ts initally load in some example snippets (mocking database request)
+</script>
+
+<div class="flex justify-center">
+	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+		<h3 class="text-center py-6">Create A Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y-4">
+			<label for="label">
+				<span>Snippet Title</span>
+				<input
+					type="text"
+					class="input"
+					placeholder="Enter title here..."
+					bind:value={formData.title}
+				/>
+			</label>
+			<label for="label">
+				<span>Programming Language</span>
+				<select class="select" bind:value={formData.language}>
+					<option value="html">HTML</option>
+					<option value="css">CSS</option>
+					<option value="typescript">Typescript</option>
+				</select>
+			</label>
+			<label for="label">
+				<span>Code Snippet</span>
+				<textarea
+					bind:value={formData.code}
+					class="textarea"
+					rows="4"
+					placeholder="Enter you snippet code here..."
+				/>
+			</label>
+		</div>
+		<div class="text-center py-6">
+			<h2>My Code Snippets</h2>
+		</div>
+		<CodeSnippetCard />
+	</div>
+</div>
